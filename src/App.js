6227628146b3e189451f2fc60f42482cd10102c2
Kurt Van Etten <kurt@vanetten.us>
2019-10-16 20:34:53 -0400

    Add CartContext provider to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 import data from './data';
 
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+
 
 function App() {
 	const [products] = useState(data);
@@ -17,21 +19,28 @@ function App() {
 		setCart([...cart, item]);
 	};
 
-
+	const removeItem = item => {
+		// removde the given item from the cart
+		// TODO: this function needs fixin' !!
+		const newCart = cart.filter(cartItem => cartItem !== item);
+		setCart(newCart);
+	};
 
 	return (
 		<ProductContext.Provider value={{ products, addItem }}>
-			<div className="App">
-				<Navigation cart={cart} />
+			<CartContext.Provider value={{cart, removeItem }}>
+				<div className="App">
+					<Navigation cart={cart} />
 
-				{/* Routes */}
-				<Route exact path="/" component={Products} />
+					{/* Routes */}
+					<Route exact path="/" component={Products} />
 
-				<Route
-					path="/cart"
-					render={() => <ShoppingCart cart={cart} />}
-				/>
-			</div>
+					<Route
+						path="/cart"
+						render={() => <ShoppingCart cart={cart} />}
+					/>
+				</div>
+			</CartContext.Provider>
 		</ProductContext.Provider>
 	);
 }
